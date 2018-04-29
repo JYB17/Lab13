0dee9123c2c8d26b4884c5c305cde0d827eb3be2
JYB17 <jyback99@naver.com>
2018-04-29 23:11:03 +0900

    prob6 done

---
+++ src/main/java/edu/handong/csee/java/lab13/prob6/prob6_main.java
@@ -0,0 +1,19 @@
+package edu.handong.csee.java.lab13.prob6;
+
+public class prob6_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UpPoint p1 = new UpPoint(2, 3);
+		DownPoint p2 = new DownPoint(2, 5);
+		DownPoint p3 = new DownPoint(3, 8);
+		UpPoint p4 = new UpPoint(6, 13);
+		
+		Printer.print(p1);
+		Printer.print(p2);
+		Printer.print(p3);
+		Printer.print(p4);
+
+	}
+
+}
