d080e3f1084527f243488512138461bdf9b33dc8
JYB17 <jyback99@naver.com>
2018-04-29 21:50:14 +0900

    prob3 done

---
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Circle.java
@@ -0,0 +1,22 @@
+package edu.handong.csee.java.lab13.prob3;
+
+public class Circle extends Shape {
+	private double radius;
+	
+	public Circle(double r) {
+		radius = r;
+	}
+	
+	public double area() {
+		return Math.PI*Math.pow(radius, 2);
+	}
+	
+	public double perimeter() {
+		return 2.0*Math.PI*radius;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+
+}
